68ae82f5bd357949f373da539a443ab27516632c
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-02-07 11:29:37 -0300

    Criando componente header e adicionando banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header"
 import NavBar from "./components/NavBar"
 
 function App() {
 
-
   return (
     <>
       <NavBar/>
+      <Header/>
     </>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import '../../styles/components/header.sass'
+
+export default function Header() {
+    return (
+        <div id='main'>
+        
+        </div>
+    )
+}
