5f58f2d40431214541930a82cb6d11d66d0d75dd
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-02-07 13:53:50 -0300

    Finalizando componente header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,14 @@ import '../../styles/components/header.sass'
 export default function Header() {
     return (
         <div id='main'>
-        
+            <div className="header-heading">
+                <h2> APRIMORE SUA </h2>
+                <h1> <span> SAÚDE </span> CONOSCO </h1>
+                <p className="details"> Desafie-se e conquiste novos patamares de bem-estar. </p>
+                <div className="header-btns">
+                    <a href="#" > JUNTE-SE </a>
+                </div>
+            </div>
         </div>
     )
 }
