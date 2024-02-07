4fbd45b55e021efadc88955c088ce61a568bc182
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-02-07 11:23:14 -0300

    Algumas configurações e criando componente NavBar

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-scroll';
+
+import '../../styles/components/navbar.sass'
+
+export default function NavBar() {
+
+    const [nav, setNav] = useState(false);
+
+    const mudarBackground = () => {
+        
+        if(window.scrollY >= 50) {
+            setNav(true);
+        } else {
+            setNav(false);
+        }
+    }
+
+    window.addEventListener('scroll', mudarBackground)
+
+    return (
+
+        <nav className={nav ? "nav active" : "nav"}>
+                
+            <Link to="#" className="logo">
+                <img src={logo} alt="" />
+            </Link>
+
+            <input type="checkbox" className="menu-btn" id="menu-btn"/>
+            <label className="menu-icon" htmlFor="menu-btn">
+                <span className="nav-icon"></span>
+            </label>
+
+            <ul className="menu">
+                <li><Link to="#"> Home </Link ></li>
+                <li><Link to="#"> Infos </Link></li>
+                <li><Link to="#"> Ofertas </Link></li>
+                <li><Link to="#"> Sobre </Link></li>
+                <li><Link to="#"> Contato </Link></li>
+            </ul>
+
+        </nav>
+
+    )
+
+}
